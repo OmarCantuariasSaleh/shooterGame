97053832b7c530cd2d77f293f9a1481cab1cba88
omarc <ryudamaru@gmail.com>
2026-05-13 23:23:10 -0400

    Initial commit

---
+++ scenes/player.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	position = Vector2(100, 500)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position += Vector2(1,0) * 50 * delta
+	$PlayerSprite.rotation += 10 * delta
