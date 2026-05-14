64979f99b661e03aaf8f7f850f61ba55cc5e4539
omarc <ryudamaru@gmail.com>
2026-05-14 00:07:25 -0400

    - added small input tests - played arround with movement and player positioning - next is to finish second chapter

--- scenes/player.gd
+++ scenes/player.gd
@@ -10,3 +10,11 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	position += Vector2(1,0) * 50 * delta
 	$PlayerSprite.rotation += 10 * delta
+	
+func _input(event: InputEvent) -> void:
+	if event.is_action("up"):
+		position += Vector2(0,-1) * 1000 * get_process_delta_time()
+		$PlayerSprite.rotation = 0
+	elif event.is_action("down"):
+			position += Vector2(0,-1) * 1000 * get_process_delta_time()
+			$PlayerSprite.rotation = 0
